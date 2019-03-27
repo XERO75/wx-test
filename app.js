8359564f385b132d5fba7fb9e9ee80ecabfe6190
xero75 <xerolry@gmail.com>
2019-03-27 18:34:30 +0800

    '微信小程序-框架'

---
+++ app.js
@@ -0,0 +1,19 @@
+App({
+  onLaunch(options) {
+    console.log('初始化');
+    console.log(options);
+  },
+  onShow(options) {
+    console.log('显示');
+    console.log(options);
+  },
+  onHide() {
+    console.log('隐藏');
+  },
+  onError(error) {
+    console.log(error);
+  },
+  globalData: {
+    greeting: ['aloha','您好','hola']
+  }
+})
